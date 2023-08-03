54df5735e75e23f91a3d4d5bb338d6a5377bb18a
elena50505vasilchenko <elena50505vasilchenko@yandex.ru>
2023-08-03 15:03:45 +0400

    Добавила min

--- task02/Program.cs
+++ task02/Program.cs
@@ -5,9 +5,9 @@ int number1 = Convert.ToInt32(Console.ReadLine());
 int number2 = Convert.ToInt32(Console.ReadLine());
 if (number1 > number2)
 {
-    Console.WriteLine("max = " + number1);
+    Console.WriteLine("max = " + number1 + " min = " + number2);
 }
 else
 {
-    Console.WriteLine("max = " + number2);
+    Console.WriteLine("max = " + number2 + " min = " + number1);
 }
