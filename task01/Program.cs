5ad6212470c594245dc150726b0f460857be09ce
elena50505vasilchenko <elena50505vasilchenko@yandex.ru>
2023-08-03 14:14:17 +0400

    Задние 2

---
+++ task01/Program.cs
@@ -0,0 +1,13 @@
+﻿
+Console.WriteLine("Введи первое число: ");
+int numder1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введи второе число: ");
+int numder2 = Convert.ToInt32(Console.ReadLine());
+if (numder1 == numder2 * numder2)
+{
+    Console.WriteLine("Первое число является квадратом второго");
+} 
+else
+{
+    Console.WriteLine("Первое число не является квадратом второго");
+}
