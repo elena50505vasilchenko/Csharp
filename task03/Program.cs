71fb8376bdcdb0862f819290ee0aa82fea1d440a
elena50505vasilchenko <elena50505vasilchenko@yandex.ru>
2023-08-03 14:14:54 +0400

    Задние 4

---
+++ task03/Program.cs
@@ -0,0 +1,32 @@
+﻿
+using System;
+Console.WriteLine("Введи номер недели: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number == 1)
+{
+    Console.WriteLine("Понедельник");
+}
+else if (number == 2)
+{
+   Console.WriteLine("Вторник"); 
+}
+else if (number == 3)
+{
+   Console.WriteLine("Среда"); 
+}
+else if (number == 4)
+{
+   Console.WriteLine("Четверг"); 
+}
+else if (number == 5)
+{
+   Console.WriteLine("Пятница"); 
+}
+else if (number == 6)
+{
+   Console.WriteLine("Суббота"); 
+}
+else if (number == 7)
+{
+   Console.WriteLine("Воскресенье"); 
+}
