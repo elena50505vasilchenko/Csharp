cbb7070a107c7f7c6075882f9a0fad4ae14352a0
elena50505vasilchenko <elena50505vasilchenko@yandex.ru>
2023-08-04 11:39:18 +0400

    Задание 11

---
+++ task11/Program.cs
@@ -0,0 +1,7 @@
+﻿// Напишите программу, которая выводит случайное трёхзначное число и удаляет вторую цифру этого числа.
+int number = new Random().Next(100, 1000);
+Console.WriteLine(number);
+int numberOne = number / 100;
+int numberThree = number % 10;
+int result = numberOne*10 + numberThree;
+Console.WriteLine(result);
