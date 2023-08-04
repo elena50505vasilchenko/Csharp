c459f4d3960edc378438a368cac660f9105f4bf0
elena50505vasilchenko <elena50505vasilchenko@yandex.ru>
2023-08-04 11:57:04 +0400

    Задание 12

---
+++ task12/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Введите два числа: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+int number2 = Convert.ToInt32(Console.ReadLine());
+int result = number1 % number2;
+if (result == 0)
+{
+    Console.WriteLine("Второе число кратное первому");
+}
+else
+{
+    Console.WriteLine($"Остаток от деления {result}");
+}
