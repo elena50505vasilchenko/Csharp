af742abf79a8f1e133164b98359185dbc6cb05ac
elena50505vasilchenko <elena50505vasilchenko@yandex.ru>
2023-08-05 13:21:48 +0400

    Задание 16

---
+++ task16/Program.cs
@@ -0,0 +1,10 @@
+﻿int number1 = Convert.ToInt32(Console.ReadLine());
+int number2 = Convert.ToInt32(Console.ReadLine());
+if(number1==number2*number2 || number2==number1*number1)
+{
+    Console.WriteLine("Да");
+}
+else
+{
+    Console.WriteLine("Нет");
+}
