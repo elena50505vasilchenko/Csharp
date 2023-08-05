8739e06488fcdadb3cd3444d62b4a3d9aeb07fa9
elena50505vasilchenko <elena50505vasilchenko@yandex.ru>
2023-08-05 13:20:57 +0400

    Задание 10

---
+++ task10/Program.cs
@@ -0,0 +1,5 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+Console.WriteLine("Введи число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int result = number / 10 % 10;
+Console.WriteLine(result);
