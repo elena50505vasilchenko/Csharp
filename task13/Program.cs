7468a64186284047a05e3bab06a321491a8312f7
elena50505vasilchenko <elena50505vasilchenko@yandex.ru>
2023-08-05 13:21:13 +0400

    Задание 13

---
+++ task13/Program.cs
@@ -0,0 +1,12 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+Console.WriteLine("Введи число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int result = number % 100 % 10;
+if (number > 1000)
+{
+    Console.WriteLine("Третьей цифры нет");
+}
+else
+{
+    Console.WriteLine(result);
+}
