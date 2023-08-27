6a9dea3b513c3d242a8c54a41caa93936e56d77c
elena50505vasilchenko <elena50505vasilchenko@yandex.ru>
2023-08-27 19:31:30 +0400

    Задание 23

---
+++ task23/Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+Console.WriteLine("Ввдите число N: ");
+int numder = Convert.ToInt32(Console.ReadLine());
+int result = 0;
+if (numder < 0)
+{
+    numder = numder * (-1);
+}
+for (int i = 1; i <= numder; i++)
+{
+    result = i * i * i;
+    Console.WriteLine(result);
+}
