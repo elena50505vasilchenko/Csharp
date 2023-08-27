39ab87966480f74f40530f5a2e3d7edb91fe7be0
elena50505vasilchenko <elena50505vasilchenko@yandex.ru>
2023-08-27 18:58:01 +0400

    Задание 22

---
+++ task22/Program.cs
@@ -0,0 +1,14 @@
+﻿
+
+Console.WriteLine("Ввдите число N: ");
+int numder = Convert.ToInt32(Console.ReadLine());
+double result = 0;
+if (numder < 0)
+{
+    numder = numder * (-1);
+}
+for (int i = 1; i <= numder; i++)
+{
+    result = Math.Pow(i, 2);
+    Console.WriteLine(result);
+}
