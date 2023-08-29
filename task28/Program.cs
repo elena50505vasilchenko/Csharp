ea6506b8d10e062f910f37e6139e584e004019cd
elena50505vasilchenko <elena50505vasilchenko@yandex.ru>
2023-08-29 17:31:36 +0400

    Добавление задания 28

---
+++ task28/Program.cs
@@ -0,0 +1,21 @@
+﻿using Internal;
+using System;
+// See https://aka.ms/new-console-template for more information
+int[] GetRandomArray() 
+{
+    int[] array = new int[8];
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(0, 2);
+    }
+    return array;
+}
+int[] userArray = GetRandomArray();
+
+void PrintArray(int[] arrayToPrit)
+{
+    for (int i = 0; i < arrayToPrit.Length; i++)
+    {
+        Console.WriteLine();
+    }
+}
