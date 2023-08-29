eb2634ed9369f9631381753c1caffecf2a3fa45f
elena50505vasilchenko <elena50505vasilchenko@yandex.ru>
2023-08-29 17:32:16 +0400

    Добавление задания 32

---
+++ task32/Program.cs
@@ -0,0 +1,47 @@
+﻿/*  Напишите программу замена элементов массива: положительные элементы замените на соответствующие отрицательные, и наоборот.
+[-4, -8, 8, 2] -> [4, 8, -8, -2] */
+
+int arrayLength = Convert.ToInt32(Console.ReadLine());
+int min = Convert.ToInt32(Console.ReadLine());
+int max = Convert.ToInt32(Console.ReadLine());
+
+
+int[] GetRandomArray(int arrayLength1, int start, int end)
+{
+    int[] array = new int[arrayLength1];
+    for (int i=0; i<array.Length; i++)
+    {
+        array [i] = new Random().Next(start, end);
+    }
+    return array;
+}
+
+int[] userArray = GetRandomArray(arrayLength, min, max);
+
+void PrintArray(int [] arrayToPrint)
+{
+    Console.Write("[");
+    for (int i=0; i<arrayToPrint.Length; i++)
+    {
+       Console.Write(arrayToPrint[i]);
+       if (i<arrayToPrint.Length - 1)
+       {
+        Console.Write (", ");
+       }
+    }
+    Console.Write("]");
+}
+int[] ChangeSignArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] *= -1;
+    }
+
+    return array;
+}
+System.Console.WriteLine();
+PrintArray(ChangeSignArray(userArray));
+
+
+
