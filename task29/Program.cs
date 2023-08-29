1d6a274751b2bde6f4bd3b5c56d324ea46f932c6
elena50505vasilchenko <elena50505vasilchenko@yandex.ru>
2023-08-29 17:31:47 +0400

    Добавление задания 29

---
+++ task29/Program.cs
@@ -0,0 +1,30 @@
+﻿//  Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+Console.WriteLine("Задайте диапазон чисел: ");
+int start = Convert.ToInt32(Console.ReadLine());
+int finish = Convert.ToInt32(Console.ReadLine());
+
+int[] GetRandomArray(int start, int finish) 
+{
+    int[] array = new int[8];
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(start, finish);
+    }
+    return array;
+}
+
+void PrintArray(int [] arrayToPrint)
+{
+    Console.Write("[");
+    for (int i=0; i<arrayToPrint.Length; i++)
+    {
+       Console.Write(arrayToPrint[i]);
+       if (i < arrayToPrint.Length - 1)
+       {
+        Console.Write (", ");
+       }
+    }
+    Console.Write("]");
+}
+
+PrintArray(GetRandomArray(start, finish));
