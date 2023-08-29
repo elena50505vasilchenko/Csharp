32d1649eb5f4e1ecbdd7177adbfe8171c1217e3e
elena50505vasilchenko <elena50505vasilchenko@yandex.ru>
2023-08-29 17:32:04 +0400

    Добавление задания 31

---
+++ task31/Program.cs
@@ -0,0 +1,57 @@
+﻿
+/* Задайте массив из 12 элементов, заполненный случайными числами из промежутка [-9, 9]. Найдите сумму отрицательных и положительных элементов массива.
+Например, в массиве [3,9,-8,1,0,-7,2,-1,8,-3,-1,6] сумма положительных чисел равна 29, сумма отрицательных равна -20. */
+int arrayL = Convert.ToInt32(Console.ReadLine());
+int min = Convert.ToInt32(Console.ReadLine());
+int max = Convert.ToInt32(Console.ReadLine());
+
+
+int[] GetRandomArray(int arrayLength1, int start, int end)
+{
+    int[] array = new int[arrayLength1];
+    for (int i=0; i<array.Length; i++)
+    {
+        array [i] = new Random().Next(start, end);
+    }
+    return array;
+}
+void PrintArray(int [] arrayToPrint)
+{
+    Console.Write("[");
+    for (int i=0; i<arrayToPrint.Length; i++)
+    {
+       Console.Write(arrayToPrint[i]);
+       if (i<arrayToPrint.Length - 1)
+       {
+        Console.Write (", ");
+       }
+    }
+    Console.Write("]");
+}
+int GetSumPlus(int[] array)
+{
+    int sum = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array [i] > 0)
+        sum += array [i];
+    }
+    return sum;
+}
+int GetSumMinus(int[] array)
+{
+    int sum = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array [i] < 0)
+        sum += array [i];
+    }
+    return sum;
+}
+
+
+int[] userArray = GetRandomArray(arrayL, min, max);
+PrintArray(userArray);
+Console.WriteLine();
+Console.WriteLine(GetSumPlus(userArray));
+Console.WriteLine(GetSumMinus(userArray));
