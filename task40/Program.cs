b2f41d4e04d874a53ce38bfae7c0f6712679a080
elena50505vasilchenko <elena50505vasilchenko@yandex.ru>
2023-09-04 12:12:58 +0400

    Добавление задания 40

---
+++ task40/Program.cs
@@ -0,0 +1,28 @@
+﻿Console.Write("Введите длины стороны: ");
+int[] FillUserArray()
+{
+    int [] array = new int[3];
+    for (int i = 0; i < 3; i++)
+    {
+        array[i] = Convert.ToInt32(Console.ReadLine());
+    }
+    return array;
+}
+bool TriangleExistance(int[] array)
+{
+    
+    
+    
+        if (array[0] < array[1] + array[2] && array[1] < array[0] + array[2] && array[2] < array[0] + array[1])
+        {
+            return true;
+        }
+        else
+        return false;
+        
+    
+
+}
+int [] userArray = FillUserArray();
+bool result = TriangleExistance(userArray);
+Console.WriteLine(result);
