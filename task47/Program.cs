05fecd6bf4ebe31f567eacb484585b7265fb568d
elena50505vasilchenko <elena50505vasilchenko@yandex.ru>
2023-09-17 23:44:33 +0400

    Добавление задания 47

---
+++ task47/Program.cs
@@ -0,0 +1,45 @@
+﻿// Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+double[,] GetRandom2DArray()
+{
+    Console.Write("Введите кол-во столбцов: ");
+    int cols = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Введите кол-во строк: ");
+    int rows = Convert.ToInt32(Console.ReadLine());
+    double[,] array = new double[rows, cols];
+    Console.Write("Введите минимальное значение: ");
+    int min = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Введите максимальное значение: ");
+    int max = Convert.ToInt32(Console.ReadLine());
+
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().NextDouble() * (max - min) + (min);
+        }
+    }
+
+    return array;
+}
+void Print2DArray(double[,] arrayForPrint)
+{
+    Console.Write($"[ ]\t");
+    for (int i = 0; i < arrayForPrint.GetLength(1); i++)
+    {
+        Console.Write($"[{i}]\t");
+    }
+    Console.WriteLine();
+
+    for (int i = 0; i < arrayForPrint.GetLength(0); i++)
+    {
+        Console.Write($"[{i}]\t");
+        for (int j = 0; j < arrayForPrint.GetLength(1); j++)
+        {
+            Console.Write(Math.Round(arrayForPrint[i, j], 2) + "\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+Print2DArray( GetRandom2DArray());
+
