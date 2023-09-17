4ec7ab458b62b05dc5b5bed82b5de4857de81c55
elena50505vasilchenko <elena50505vasilchenko@yandex.ru>
2023-09-17 23:44:04 +0400

    Добавление задания 44

---
+++ task44/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Ввдите число N: ");
+int a = Convert.ToInt32(Console.ReadLine());
+int[] array = new int [a];
+void Fibonacci(int[] array)
+{
+    array[0] = 0;
+    array[1] = 1;
+    for (int i = 2; i < array.Length; i++)
+    {
+        array[i] = array[i-1] + array[i+2];
+        Console.WriteLine(array[i]);
+    }
+    Console.Write("]");
+}
+
+Fibonacci(array);
