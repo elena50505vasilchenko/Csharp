d5361423cdb3eb70971c46a5ac3bc1205a8df41c
elena50505vasilchenko <elena50505vasilchenko@yandex.ru>
2023-09-17 23:44:24 +0400

    Добавление задания 46

---
+++ task46/Program.cs
@@ -0,0 +1,35 @@
+﻿
+// Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+int[,] GetRandom2DArray(int rowNumber, int columnNumber, int deviation)
+ {
+    int[,] result = new int[rowNumber,columnNumber];
+    for (int i = 0; i < rowNumber; i++)
+    {
+        for (int j = 0; j < columnNumber; j++)
+        {
+            result[i,j] = new Random().Next(- deviation, deviation);
+        }
+    }
+    return result;
+ }
+
+ void Print2DArray(int[,] arrayToPrint)
+ {
+    System.Console.Write($"[ ]\t");
+    for (int i = 0; i < arrayToPrint.GetLength(1); i++)
+        {
+            Console.Write($"[{i}] \t");
+        }
+        System.Console.WriteLine();
+    for (int i = 0; i < arrayToPrint.GetLength(0); i++)
+    {
+        Console.Write($"[{i}] \t");
+        for (int j = 0; j < arrayToPrint.GetLength(1); j++)
+        {
+            Console.Write($"{arrayToPrint[i,j]}\t");
+        }
+    Console.WriteLine();
+    }
+ }
+
+ Print2DArray(GetRandom2DArray(5,5,10));
