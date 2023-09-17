b6385d7d9a8f604dbca8506b477f197a0ecaf220
elena50505vasilchenko <elena50505vasilchenko@yandex.ru>
2023-09-17 23:44:42 +0400

    Добавление задания 48

---
+++ task48/Program.cs
@@ -0,0 +1,34 @@
+﻿// See https://aka.ms/new-console-template for more information
+int[,] Get2DArray(int m, int n)
+{
+    int[,] array = new int[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            array[i, j] = i + j;
+        }
+    }
+    return array;
+}
+void Print2DArray(int[,] arrayToPrint)
+{
+    System.Console.Write($"[ ]\t");
+    for (int i = 0; i < arrayToPrint.GetLength(1); i++)
+    {
+        Console.Write($"[{i}]\t");
+    }
+     System.Console.WriteLine();
+    for (int i = 0; i < arrayToPrint.GetLength(0); i++)
+    {
+        Console.Write($"[{i}]\t");
+        for (int j = 0; j < arrayToPrint.GetLength(1); j++)
+        {
+            System.Console.Write(arrayToPrint[i,j] +"\t");
+        }
+        System.Console.WriteLine();
+    }
+}
+
+int [,] arr = Get2DArray(5,10);
+Print2DArray(arr);
