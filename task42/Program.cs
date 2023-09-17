79b97934912074f3951f20145a45e8149fcd2f5a
elena50505vasilchenko <elena50505vasilchenko@yandex.ru>
2023-09-17 23:43:44 +0400

    Добавление задания 42

---
+++ task42/Program.cs
@@ -0,0 +1,17 @@
+﻿
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Ввдите число N: ");
+int a = Convert.ToInt32(Console.ReadLine());
+string Delenie(int a)
+{
+    string result = String.Empty;
+    int temp = 0;
+    for (int i = a; i > 0; i/=2)
+    {
+        temp = i % 2;
+        result += temp;
+    }
+    return result;
+}
+string b = Delenie(a);
+Console.WriteLine(b);
