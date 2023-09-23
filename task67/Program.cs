4b91bc2f516e4e563f0ec2ad2a6128813f1d4fdc
elena50505vasilchenko <elena50505vasilchenko@yandex.ru>
2023-09-23 13:22:38 +0400

    Добавление задания 67

---
+++ task67/Program.cs
@@ -0,0 +1,12 @@
+﻿/* 67. Напишите программу, которая будет принимать на вход число и возвращать сумму его цифр.
+*/
+int result = SumNumber(246);
+
+int SumNumber (int n)
+{   
+    if (n==0)
+    return 0;
+    else
+    return n%10+SumNumber(n/10);
+}
+System.Console.WriteLine(result);
