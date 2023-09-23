44526fd7c560a662f3372e379c125c3f25070378
elena50505vasilchenko <elena50505vasilchenko@yandex.ru>
2023-09-23 13:21:45 +0400

    Добавление задания 63

---
+++ task63/Program.cs
@@ -0,0 +1,12 @@
+﻿/* 63. Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от 1 до N.
+*/
+
+void NuturalNum(int n, int start = 1)
+{
+    if (start <= n)
+    {
+        System.Console.Write(start+", ");
+        NuturalNum(n, start + 1);
+    }
+}
+NuturalNum(5);
