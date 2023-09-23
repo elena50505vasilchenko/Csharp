718f26ea244d1133aa37e30559281f4100596cc7
elena50505vasilchenko <elena50505vasilchenko@yandex.ru>
2023-09-23 13:22:12 +0400

    Добавление задания 65

---
+++ task65/Program.cs
@@ -0,0 +1,12 @@
+﻿/* 65. Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N.
+*/
+
+void NuturalNum(int m, int n)
+{
+    if (m <= n)
+    {
+        System.Console.Write(m+", ");
+        NuturalNum(m+1 , n);
+    }
+}
+NuturalNum(1,7);
